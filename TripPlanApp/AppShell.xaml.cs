6087ba4b22b9f8e6a9c7decb6b2447840113431c
yaron <yaron@YARONIX>
2024-11-06 18:14:54 +0200

    a

--- TripPlanApp/AppShell.xaml.cs
+++ TripPlanApp/AppShell.xaml.cs
@@ -1,9 +1,17 @@
-﻿namespace TripPlanApp
+﻿using TripPlanApp.ViewModels;
+
+namespace TripPlanApp
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
+            InitializeComponent();
+        }
+
+        public AppShell(ShellViewModel vm)
+        {
+            this.BindingContext = vm;
             InitializeComponent();
         }
     }
