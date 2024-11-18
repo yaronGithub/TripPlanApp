2afa90cce5cb0cebefab75ef91ed5f84a077c50e
User <User@LAB3-3>
2024-11-18 14:21:47 +0200

    sqS

--- TripPlanApp/ViewModels/UserPageViewModel.cs
+++ TripPlanApp/ViewModels/UserPageViewModel.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using TripPlanApp.Services;
+using TripPlanApp.Views;
 
 namespace TripPlanApp.ViewModels
 {
     public class UserPageViewModel : ViewModelBase
     {
         TripPlanWebAPIProxy proxy;
-        public UserPageViewModel(TripPlanWebAPIProxy proxy) 
+        private IServiceProvider serviceProvider;
+        public UserPageViewModel(TripPlanWebAPIProxy proxy, IServiceProvider serviceProvider)
         {
             this.proxy = proxy;
+            this.serviceProvider = serviceProvider;
+            AddPlanningCommand = new Command(OnAddPlanning);
+        }
+
+        public ICommand AddPlanningCommand { get; }
+
+        private async void OnAddPlanning()
+        {
+            ((App)Application.Current).MainPage.Navigation.PushAsync(serviceProvider.GetService<PlanningPageView>());
         }
     }
 }
