949472fd645f332ff09cf601c4d66b88458cdc38
User <User@LAB3-3>
2024-11-25 13:52:27 +0200

    aaaaaaaaa

--- TripPlanApp/ViewModels/PlanningPageViewModel.cs
+++ TripPlanApp/ViewModels/PlanningPageViewModel.cs
@@ -36,6 +36,174 @@ namespace TripPlanApp.ViewModels
             CancelCommand = new Command(Cancel);
         }
 
+
+        //Define properties for each field in the task form including error messages and validation logic
+        #region PlanningDescription
+        private bool showTaskDescriptionError;
+        public bool ShowTaskDescriptionError
+        {
+            get => showTaskDescriptionError;
+            set
+            {
+                showTaskDescriptionError = value;
+                OnPropertyChanged(nameof(ShowTaskDescriptionError));
+            }
+        }
+        private string taskDescription;
+        public string TaskDescription
+        {
+            get => taskDescription;
+            set
+            {
+                taskDescription = value;
+                ValidateTaskDescription();
+                OnPropertyChanged(nameof(TaskDescription));
+            }
+        }
+        private string taskDescriptionError;
+        public string TaskDescriptionError
+        {
+            get => taskDescriptionError;
+            set
+            {
+                taskDescriptionError = value;
+                OnPropertyChanged(nameof(TaskDescriptionError));
+            }
+        }
+        public void ValidateTaskDescription()
+        {
+            this.TaskDescriptionError = "Description is required";
+            this.ShowTaskDescriptionError = string.IsNullOrEmpty(TaskDescription);
+        }
+
+        #endregion PlanningDescription
+
+        #region PlanningDueDate
+        private bool showTaskDueDateError;
+        public bool ShowTaskDueDateError
+        {
+            get => showTaskDueDateError;
+            set
+            {
+                showTaskDueDateError = value;
+                OnPropertyChanged(nameof(ShowTaskDueDateError));
+            }
+        }
+        private DateOnly taskDueDate;
+        public DateTime TaskDueDate
+        {
+            get => taskDueDate.ToDateTime(TimeOnly.MinValue);
+            set
+            {
+                taskDueDate = new DateOnly(value.Year, value.Month, value.Day);
+                ValidateTaskDueDate();
+                OnPropertyChanged(nameof(TaskDueDate));
+            }
+        }
+
+        private string taskDueDateError;
+        public string TaskDueDateError
+        {
+
+            get => taskDueDateError;
+            set
+            {
+                taskDueDateError = value;
+                OnPropertyChanged(nameof(TaskDueDateError));
+            }
+        }
+        public void ValidateTaskDueDate()
+        {
+            this.ShowTaskDueDateError = taskDueDate < DateOnly.FromDateTime(DateTime.Now);
+        }
+        #endregion PlanningDueDate
+        #region PlanningActualDate
+        private bool showPlanningActualDateError;
+        public bool ShowPlanningActualDateError
+        {
+            get => showPlanningActualDateError;
+            set
+            {
+                showPlanningActualDateError = value;
+                OnPropertyChanged(nameof(ShowPlanningActualDateError));
+            }
+        }
+        private DateOnly? planningActualDate;
+        public DateTime? PlanningActualDate
+        {
+            get
+            {
+                if (planningActualDate == null)
+                    return null;
+                else
+                {
+                    DateOnly val = planningActualDate.Value;
+                    return val.ToDateTime(TimeOnly.MinValue);
+                }
+
+            }
+            set
+            {
+                if (value == null)
+                    planningActualDate = null;
+                else
+                {
+                    DateTime val = value.Value;
+                    planningActualDate = new DateOnly(val.Year, val.Month, val.Day);
+                }
+                ValidatePlanningActualDate();
+                OnPropertyChanged(nameof(PlanningActualDate));
+            }
+        }
+        private string taskActualDateError;
+        public string TaskActualDateError
+        {
+            get => taskActualDateError;
+            set
+            {
+                taskActualDateError = value;
+                OnPropertyChanged(nameof(TaskActualDateError));
+            }
+        }
+        public void ValidatePlanningActualDate()
+        {
+            this.ShowPlanningActualDateError = false;
+
+        }
+        #endregion PlanningActualDate
+
+        #region Places collection
+        //Define an ObservableCollection of TaskComment to hold the comments for the task
+        private ObservableCollection<PlanPlace> planningPlaces;
+        public ObservableCollection<PlanPlace> PlanningPlaces
+        {
+            get => planningPlaces;
+            set
+            {
+                planningPlaces = value;
+                OnPropertyChanged(nameof(PlanningPlaces));
+            }
+        }
+        //Define a property to hold the new comment text
+        private string newComment;
+        public string NewComment
+        {
+            get => newComment;
+            set
+            {
+                newComment = value;
+                OnPropertyChanged(nameof(NewComment));
+                OnPropertyChanged(nameof(EnableNewCommentButton));
+            }
+        }
+        //define a boolean property to indiczte if the new comment field is not empty
+        public bool EnableNewCommentButton
+        {
+            get => !string.IsNullOrEmpty(NewComment);
+        }
+
+        #endregion Places collection
+
         #region Save Planning
         //define a command to save the task
         public Command SavePlanningCommand { get; set; }
@@ -45,17 +213,17 @@ namespace TripPlanApp.ViewModels
             //Validate the task fields
             ValidateTaskDescription();
             ValidateTaskDueDate();
-            ValidateTaskActualDate();
+            ValidatePlanningActualDate();
             //If there are errors, return
-            if (ShowTaskDescriptionError || ShowTaskDueDateError || ShowTaskActualDateError)
+            if (ShowTaskDescriptionError || ShowTaskDueDateError || ShowPlanningActualDateError)
                 return;
             InServerCall = true;
             PlanGroup? updatedUserPlanning = new PlanGroup();
             updatedUserPlanning.PlanId = planning.PlanId;
             updatedUserPlanning.GroupDescription = TaskDescription;
             updatedUserPlanning.TaskDueDate = taskDueDate;
-            updatedUserPlanning.TaskActualDate = taskActualDate;
-            updatedUserPlanning.TaskComments = TaskComments.ToList();
+            updatedUserPlanning.TaskActualDate = planningActualDate;
+            updatedUserPlanning.TaskComments = PlanningPlaces.ToList();
             updatedUserPlanning.UserId = ((App)Application.Current).LoggedInUser.UserId;
             //If the task is new, add it to the database
             if (planning.PlanId == 0)
